3de5668ddffb1d8b9a02d93d5fbcf2a89dd534dc
Your Name <you@example.com>
2022-04-07 09:46:12 +0000

    gala

--- git-v1.sh
+++ git-v1.sh
@@ -5,6 +5,19 @@
 echo "Checking for newer files online first"
 git pull
 
+echo "What is your GPG key? (empty: none)"
+read gpg_key
+
+rm x86_64/*.pkg.tar.*sig
+if ! [[ $gpg_key == "none" ]]; then
+    for i in $(ls --ignore="*.sig" --ignore="*.sh" --ignore="*.db*" --ignore="*.files*" x86_64/); do
+        gpg --local-user ${gpg_key} --yes --detach-sign --use-agent "x86_64/${i}"
+    done
+else
+    echo "No GPG key entered!"
+fi
+
+rm -f x86_64/tearch_repo.*
 ./x86_64/update.sh
 
 # Below command will backup everything inside the project folder
